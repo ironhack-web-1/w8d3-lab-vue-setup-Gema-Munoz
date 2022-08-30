d755794694a5c6b605d250bd90b9e44d37839ce3
gema-munoz <gema.m.moscoso@gmail.com>
2022-08-30 21:06:05 +0200

    prueba

---
+++ hello-world/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Vue 3!
+    <About /> 
+  </div>
+</template>
+
+<script>
+import About from 'About.vue'
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
